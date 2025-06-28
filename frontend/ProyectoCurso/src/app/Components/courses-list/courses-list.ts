97dcce56a544e2f1c5f30eb43e5f4fb83d2f5670
july <leonjairetsell@gmail.com>
2025-06-28 11:22:25 -0600

    Angular Material Custom

--- frontend/ProyectoCurso/src/app/Components/courses-list/courses-list.ts
+++ frontend/ProyectoCurso/src/app/Components/courses-list/courses-list.ts
@@ -1,24 +1,21 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { CoursesService, Course } from '../../Services/courses.service';
 import {MatCardModule} from '@angular/material/card';
-import { catchError, of } from 'rxjs';
-import { toSignal } from '@angular/core/rxjs-interop';
-import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-courses-list',
-  imports: [MatCardModule, HttpClientModule],
+  imports: [MatCardModule],
   templateUrl: './courses-list.html',
   styleUrl: './courses-list.css'
 })
+
 export class CoursesList{
   private service=inject(CoursesService);
+  courses=signal(<Course[]>[]);
+    
+  ngOnInit() {
+    this.service.getCourses();
+   }
 
-  courses=toSignal(this.service
-    .getCourses()
-    .pipe(catchError(err=> {
-      console.log(err);
-      return of([])
-    })),{initialValue: [] as Course[]});
 
 }
